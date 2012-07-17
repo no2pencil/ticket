c48a3a5152dfdc464f65ecf2853e30209fc281ec
Sean Grant <sean.grant@ymail.com>
2012-07-16 21:27:41 -0700

    Moved login pages to /v/, merged /css/ with /v/css/

--- inc/startup.php
+++ inc/startup.php
@@ -7,6 +7,6 @@ require_once("autoload.php"); // Autoload so $framework functions work
 
 if(!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] == false){
 	// Todo: login scripts
-	require_once("login.php");
+	require_once("../v/login.php");
 }
 ?>
