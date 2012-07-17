5787b9c6e0bb0011b51a455c81f7fe4d552b560e
Sean Grant <sean.grant@ymail.com>
2012-07-16 21:22:54 -0700

    Modified startup.php to prevent breaking

--- inc/startup.php
+++ inc/startup.php
@@ -2,12 +2,11 @@
 if(!isset($_SESSION)){
 	session_start();
 }
+
+require_once("autoload.php"); // Autoload so $framework functions work
+
 if(!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] == false){
 	// Todo: login scripts
 	require_once("login.php");
-}
-else {
-	// Todo: ???
-	require_once("autoload.php");
 }
 ?>
