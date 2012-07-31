bd50805ecd7b00441a87288d9f969506971f433a
Jason Goretzki <jason@akroncdnr.com>
2012-07-31 03:47:57 -0400

    Added Twitter BootStrap... sort of

--- v/index.php
+++ v/index.php
@@ -8,14 +8,23 @@
 		<script type="text/javascript" src="v/js/jquery-1.7.2.min.js"></script>
 		<script type="text/javascript" src="v/js/jquery-ui-1.8.21.custom.min.js"></script>
 		<script type="text/javascript" src="v/js/scripts.js"></script>
+    		<link href="v/css/bootstrap/bootstrap.css" rel="stylesheet"> 
+		<link href="v/css/bootstrap/bootstrap-responsive.css" rel="stylesheet"> 
+		<link href="v/css/bootstrap/docs.css" rel="stylesheet"> 
 	</head>
 	<body>
 		<div class="wrapper">
 			<div class="leftMenu">
-				<a href="index.php">Home</a>
-				<a href="customers.php">Customers</a>
-				<a href="tickets.php">Tickets</a>
-				<a href="about.php">About</a>
+				<form> 
+				<a class="btn" href="index.php">Home</a> 
+				<a class="btn" href="customers.php">Customers</a>
+				<a class="btn" href="tickets.php">Tickets</a>
+				<a class="btn" href="about.php">About</a>
+<a class="btn" href="">Link</a> 
+<button class="btn" type="submit">Button</button> 
+<input class="btn" type="button" value="Input"> 
+<input class="btn" type="submit" value="Submit"> 
+				</form>
 			</div>
 			<div class="content">
 				<?php
