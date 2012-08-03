a04f9caf66e5f3e06422f1bff8de207d641fafae
Jason Goretzki <jason@akroncdnr.com>
2012-08-03 14:23:30 -0400

    Fixed something that was trashing the display in index

--- v/index.php
+++ v/index.php
@@ -1,20 +1,19 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-		<title>Ticketing system</title>
-		<link rel="stylesheet" href="v/css/style.css" type="text/css">
-		<link type="text/css" href="v/css/jquery-ui/jquery-ui-1.8.21.custom.css" rel="stylesheet" />
-		<script type="text/javascript" src="v/js/jquery-1.7.2.min.js"></script>
-		<script type="text/javascript" src="v/js/jquery-ui-1.8.21.custom.min.js"></script>
-		<script type="text/javascript" src="v/js/scripts.js"></script>
-		<script type="text/javascript" src="v/css/bootstrap/docs/assets/js/bootstrap-dropdown.js"></script>
-		<link href="v/css/bootstrap/docs/assets/css/bootstrap.css" rel="stylesheet"> 
-		<link href="v/css/bootstrap/docs/assets/css/bootstrap-responsive.css" rel="stylesheet">
-		<link href="v/css/bootstrap/docs/assets/js/google-code-prettify/prettify.css" rel="stylesheet"> 
-	</head>
-	<body>
-
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+  <title>Ticketing system</title>
+  <link rel="stylesheet" href="v/css/style.css" type="text/css">
+  <link type="text/css" href="v/css/jquery-ui/jquery-ui-1.8.21.custom.css" rel="stylesheet" />
+  <script type="text/javascript" src="v/js/jquery-1.7.2.min.js"></script>
+  <script type="text/javascript" src="v/js/jquery-ui-1.8.21.custom.min.js"></script>
+  <script type="text/javascript" src="v/js/scripts.js"></script>
+  <script type="text/javascript" src="v/css/bootstrap/docs/assets/js/bootstrap-dropdown.js"></script>
+  <link href="v/css/bootstrap/docs/assets/css/bootstrap.css" rel="stylesheet"> 
+  <link href="v/css/bootstrap/docs/assets/css/bootstrap-responsive.css" rel="stylesheet">
+  <link href="v/css/bootstrap/docs/assets/js/google-code-prettify/prettify.css" rel="stylesheet"> 
+</head>
+<body>
 <div class="navbar">
   <div class="navbar-inner">
     <div class="container">
