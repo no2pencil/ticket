78093b3737e3c32627f6190c6e1a3d1ed9523bf7
Sean Grant <sean.grant@ymail.com>
2012-10-21 16:46:19 -0700

    Updated table function to automatically calculate widths like in browsers

--- inc/pdf.class.php
+++ inc/pdf.class.php
@@ -15,21 +15,33 @@ class pdf extends framework {
      * invoice($invoice_number)
      * 
      */
-    public function invoice($invoice_number){
+    public function invoice($invoice_number){ 
         $pdf = $this->getFPDF();
         if(!$pdf){
             return false;
         }
         $pdf->AliasNbPages();
         $pdf->AddPage();
-        $pdf->SetFont('Arial', 'B', 16);
+        $pdf->SetFont('Arial', 'B', 23);
         $pdf->Cell(0, 10, 'INVOICE', 'B', 1, 'C');
         $pdf->Ln(10);
         
         $pdf->SetFont('Arial', '', '13');
         $cols = array(
-            array("Item", 40, "L"),
-            array("Price", 30, "R")
+            array("Order Number", "C"),
+            array("Invoice Date", "C"),
+            array("Delivered Date", "C"),
+            array("Delivered Via", "C")
+        );
+        $data = array(
+            array(1234, date('m/d/y'), '11/11/11', 'Email: Business@name.com')
+        );
+        $this->table($pdf, $cols, $data);
+        $pdf->Ln(5);
+        
+        $cols = array(
+            array("Item", "L"),
+            array("Price", "R")
         );
         $data = array(
             array("Web design", "$99"),
@@ -48,22 +60,44 @@ class pdf extends framework {
     /*
      * table(fpdf $fpdf, array $header, array $data)
      * Creates a table in $fpdp with the data provided
-     * header should contain arrays like: array(TITLE, WIDTH, TEXT-ALIGN) - Example: array('price', 40, 'R')
+     * header should contain arrays like: array(TITLE, TEXT-ALIGN) - Example: array('price', 'R')
      * Data should also contain arrays inside of it's array. The arrays inside should contain the same amount of values as there are columns.
      */
     private function table($pdf, $header, $data){
-        foreach($header as $col){
-            $pdf->Cell($col[1], 7, $col[0], 1, 0, $col[2]);
-        }
-        $pdf->Ln();
+        // Calculate widths
         foreach($data as $row){
             foreach($row as $key => $col){
-                $pdf->Cell($header[$key][1], 7, $col, 'LR', 0, $header[$key][2]);
+                $dat_w = $pdf->GetStringWidth($col);
+                $col_w = $pdf->GetStringWidth($header[$key][0]);
+                if($dat_w >= $col_w){
+                    if($header[$key][2] < ($dat_w + 5)){
+                        $header[$key][2] = $dat_w + 5;
+                    }
+                } else {
+                    if($header[$key][2] < ($col_w + 5)){
+                        $header[$key][2] = $col_w + 5;
+                    }
+                }
+            }
+        }
+        
+        // Draw header
+        foreach($header as $key => $col){
+            $pdf->Cell($col[2], 7, $col[0], 1, 0, $col[1]);
+        }
+        $pdf->Ln();
+        
+        // Draw data
+        foreach($data as $row){
+            foreach($row as $key => $col){
+                $pdf->Cell($header[$key][2], 7, $col, 'LR', 0, $header[$key][1]);
             }
             $pdf->Ln();
         }
+        
+        // Bottom line
         foreach($header as $h){
-            $width += $h[1];
+            $width += $h[2];
         }
         $pdf->Cell($width, 0, '', 'T');
     }
