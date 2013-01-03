78a86fc2961b09a85f32cbd3be05c6f561c0c899
Jason Goretzki <jason@akroncdnr.com>
2013-01-03 12:14:46 -0500

    Added classy pagination

--- m/index.php
+++ m/index.php
@@ -19,18 +19,44 @@ foreach($tickets as $ticket){
         $page = (isset($_GET['page'])) ? (int)$_GET['page'] : 0;
         $data = $framework->get('tickets')->getBulkOpen(25, $page);
         $content .= '<h3>Viewing current open tickets</h3>';
+
+        if($page<1){
+                $previousBtn = '<li class="disabled">
+                <a href="#">Previous</a>
+                </li>';
+        } else {
+                $previousBtn = '<li>
+                <a href="index.php?&page=' . ($page-1) . '">Previous</a>
+                </li>';
+        }
+
         if(empty($data)){
+		$nextBtn = '<li class="disabled">
+			<a href="#">Next</a>
+		</li>';
                 $content .= '
                 <div class="alert alert-error">
                 <strong>There are no more tickets</strong> | <a href="javascript:history.go(-1)">Go Back</a>
-                </div>';
+		'.$nextBtn.'<div>';
         } else {
                 $content .= $framework->get('tickets')->generateListDisplay($data);
+		$nextBtn = '<li>
+			<a href="index.php?&page=' . ($page+1) . '">Next</a>
+		</li>';
 	}
 
+        $content .= '                   
+                <div class="well">
+                        <ul class="pager">
+                                ' . $previousBtn . $nextBtn . '
+                        </ul>
+                </div>';
+
+/*
 $content .='<h5>There are ';
-//$content .= $ticketcount;
+$content .= $ticketcount;
 $content .= ' tickets. ';
-//$content .= $ticketopen;
+$content .= $ticketopen;
 $content .= ' of which are not closed.</h5>';
+*/
 ?>
