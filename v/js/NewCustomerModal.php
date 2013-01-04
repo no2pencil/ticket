2e757c34761652c6de717c8e9155430c96c2e2a5
Jason Goretzki <jason@akroncdnr.com>
2013-01-03 20:18:00 -0500

    Moved Modals to js

---
+++ v/js/NewCustomerModal.php
@@ -0,0 +1,62 @@
+<div id="NewCustomerModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="NewCustomerModalLabel" aria-hi
+dden="true">
+  <div class="modal-header">
+    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">\xd7</button>
+    <h3 id="NewCustomerModalLabel">New Customer</h3>
+  </div>
+  <div class="modal-body">
+    <form action="customers.php" method="post" class="form-horizontal">
+      <input type="hidden" name="savenew" value="true">
+      <div class="control-group">
+        <label class="control-label">Name</label>
+        <div class="controls">
+          <input type="text" name="name">
+        </div>
+      </div>
+      <div class="control-group">
+        <label class="control-label">Email</label>
+        <div class="controls">
+          <input type="text" name="email">
+        </div>
+      </div>
+      <div class="control-group">
+        <label class="control-label">Primary phone</label>
+        <div class="controls">
+          <input type="text" name="primaryPhone">
+        </div>
+      </div>
+      <div class="control-group">
+        <label class="control-label">Secondary phone</label>
+        <div class="controls">
+          <input type="text" name="secondaryPhone">
+        </div>
+      </div>
+      <div class="control-group">
+        <label class="control-label">Address</label>
+        <div class="controls">
+          <input type="text" name="address">
+        </div>
+      </div>
+      <div class="control-group">
+        <label class="control-label">Referral</label>
+        <!-- <select name="referral" data-placeholder="Referrals" class="chzn-select search-query span3"> -->
+        <div class="controls">
+          <select name="referral">
+          <?php
+            $Referrals = $framework->get('customers')->getReferrals();
+            if($Referrals) {
+              foreach($Referrals as $Referral) {
+                echo '<option value='.$Referral['reff.id'].'>'.$Referral['reff.reff'].'</option>';
+              }
+            }
+          ?>
+          </select>
+        </div>
+      </div>
+  </div>
+  <div class="modal-footer">
+    <button class="btn btn-primary">Save</button>
+    <button class="btn btn-warning" data-dismiss="modal" aria-hidden="true">Cancel</button>
+  </div>
+  </form>
+</div>
---
+++ v/js/ReferralsModal.php
@@ -0,0 +1,18 @@
+<div id="ReferralsModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="ReferralsModalLabel" aria-hidden="false">
+  <form action="" method="post" class="form-horizontal">
+  <div class="modal-header">
+    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">x</button>
+    <h3 id="ReferralsModalLabel">Referrals Management</h3>
+  </div>
+  <div class="modal-body">
+    <input type="hidden" name="new" value="process">
+    <fieldset>
+    </fieldset>
+  </div>
+  <div class="modal-footer">
+    <button class="btn btn-primary">Save</button>
+    <button class="btn btn-warning" data-dismiss="modal" aria-hidden="true">Cancel</button>
+  </div>
+  </form>
+</div>
+
