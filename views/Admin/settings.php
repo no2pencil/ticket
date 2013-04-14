f635e65b5c6c4edb785f4347eec0c38a04f92dd5
Sean Grant <sean.grant@ymail.com>
2013-04-13 20:21:42 -0700

    Settings with the same categories are now put together. Settings without a category go under 'Other'

--- views/Admin/settings.php
+++ views/Admin/settings.php
@@ -3,15 +3,31 @@
 <form action="<?php echo _SITE_ROOT; ?>/Admin/settings/save" method="post" class="form-horizontal">
 	<fieldset>
 		<?php
+		/* Group all categories together so we can display them appropriately */
 		foreach($view->settings as $setting){
-		?>
-			<div class="control-group">
-				<label class="control-label"><?php echo $setting['keyname']; ?></label>
-				<div class="controls">
-					<input class="input-block-level" type="text" name="<?php echo str_replace(' ', '_', $setting['keyname']); ?>" value="<?php echo $setting['keyvalue']; ?>"/><?php if(!empty($setting['helptext'])){ ?><span class="help-inline"><?php echo $setting['helptext']; ?></span><?php } ?>
+			if(empty($settings['category'])){
+				$settings['category'] = 'Other';
+			}
+			if(!isset($categories[$settings['category']])){
+				$categories[$settings['category']] = array();
+			}
+			$categories[$settings['category']][] = $setting;
+		}
+
+		/* Display these settings */
+		foreach($categories as $category => $values){
+			?><legend><?php echo $category; ?></legend>
+			<?php
+			foreach($values as $setting){ 
+			?>
+				<div class="control-group">
+					<label class="control-label"><?php echo $setting['keyname']; ?></label>
+					<div class="controls">
+						<input class="span6" type="text" name="<?php echo str_replace(' ', '_', $setting['keyname']); ?>" value="<?php echo $setting['keyvalue']; ?>"/><?php if(!empty($setting['helptext'])){ ?><span class="help-inline"><?php echo $setting['helptext']; ?></span><?php } ?>
+					</div>
 				</div>
-			</div>
 		<?php
+			}
 		}
 		?>
 		<div class="form-actions">
