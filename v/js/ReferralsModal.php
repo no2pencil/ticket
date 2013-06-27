bba63029ef705739ca73e74b1d9b7162b0f7699f
Jason Goretzki <jason@akroncdnr.com>
2013-06-27 01:54:58 -0400

    Added Referral List, no actions yet

--- v/js/ReferralsModal.php
+++ v/js/ReferralsModal.php
@@ -5,11 +5,20 @@
     <h3 id="ReferralsModalLabel">Referrals Management</h3>
   </div>
   <div class="modal-body">
-    <input type="hidden" name="new" value="process">
     <fieldset>
+    <table class="table table-striped">
+    <tr><td>#</td><td>Referral</td></tr>
+    <?php
+      $referrals = $framework->get('customers')->getReferrals();
+      foreach($referrals as $referral) {
+        echo '<tr><td>'.$referral['reff.id'].'</td><td>'.$referral['reff.reff'].'</td></tr>';
+      }
+    ?>
+    </table>
     </fieldset>
   </div>
   <div class="modal-footer">
+    <input type="hidden" name="new" value="process">
     <button class="btn btn-primary">Save</button>
     <button class="btn btn-warning" data-dismiss="modal" aria-hidden="true">Cancel</button>
   </div>
